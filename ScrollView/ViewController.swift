06a017410677f1da74c40be792c1f1cd350b6d3d
student <student@iMac-73.local>
2018-12-18 11:57:48 +0800

    first

--- ScrollView/ViewController.swift
+++ ScrollView/ViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIScrollViewDelegate {
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControll: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+//        scrollView.minimumZoomScale = 0.2
+//        scrollView.maximumZoomScale = 5
+        scrollView.delegate = self
+        
+        for i in 1...7 {
+            let imageView = UIImageView(image: UIImage(named: "\(i)"))
+            imageView.contentMode = .scaleAspectFit
+            imageView.frame = CGRect(x: CGFloat(i-1) * scrollView.bounds.width, y: 0, width: scrollView.bounds.width, height: scrollView.bounds.height)
+            scrollView.addSubview(imageView)
+        }
 
+        scrollView.contentSize = CGSize(width: scrollView.bounds.width * 7, height: scrollView.bounds.height)
+        scrollView.isPagingEnabled = true
+        scrollView.showsHorizontalScrollIndicator = false
+        
+        pageControll.numberOfPages = 7
+        pageControll.currentPage = 0
+        
+    }
+    
+    @IBAction func pageControllClicked(_ sender: UIPageControl) {
+        let currentPage = sender.currentPage
+        let rect = CGRect(x: CGFloat(currentPage) * scrollView.bounds.width, y: 0, width: scrollView.bounds.width, height: scrollView.bounds.height)
+        scrollView.scrollRectToVisible(rect, animated: true)
+        
+    }
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let currentPage = scrollView.contentOffset.x/scrollView.bounds.width
+        pageControll.currentPage = Int(currentPage)
+    }
 
 }
 
